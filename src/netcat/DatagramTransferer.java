414b8c8175938297e18dd44b53217e60223c0eb3
Pavel Derendyaev <pderendyaev@smile-net.ru>
2015-08-20 13:28:15 +0300

    Compile as Java-8, add UDP support, bump to 1.1

---
+++ src/netcat/DatagramTransferer.java
@@ -0,0 +1,76 @@
+package netcat;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+public class DatagramTransferer implements Runnable {
+
+    // Ready to get full-size UDP datagram or TCP segment in one step
+    public static int RECEIVE_BUFFER_LIMIT = 2 << 16 - 1;
+
+    // Ethernet-safe buffer limit
+    public static int SEND_BUFFER_LIMIT = 1400;
+
+    private static InetSocketAddress remoteAddress;
+
+    private InputStream input;
+    private OutputStream output;
+    private DatagramChannel channel;
+
+    public DatagramTransferer(InputStream input, DatagramChannel channel) {
+        this.input = input;
+        this.channel = channel;
+    }
+
+    public DatagramTransferer(DatagramChannel channel, OutputStream output) {
+        this.channel = channel;
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        ByteBuffer buf = ByteBuffer.allocate(input != null ? SEND_BUFFER_LIMIT : RECEIVE_BUFFER_LIMIT);
+        try {
+            while (true) {
+                if (input != null) {
+                    send(buf);
+                } else {
+                    receive(buf);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int send(ByteBuffer buf) throws IOException {
+        int bytesSent = 0;
+        buf.clear();
+        int bytesRead = input.read(buf.array());
+        if (bytesRead > 0) {
+            bytesSent = channel.send(ByteBuffer.wrap(buf.array(), 0, bytesRead), remoteAddress);
+        }
+        return bytesSent;
+    }
+
+    private int receive(ByteBuffer buf) throws IOException {
+        int bytesReceived;
+        buf.clear();
+        if (!channel.isConnected()) {
+            InetSocketAddress oldRemoteAddress = remoteAddress;
+            remoteAddress = (InetSocketAddress) channel.receive(buf);
+            if (remoteAddress != null && !remoteAddress.equals(oldRemoteAddress)) {
+                System.err.println(String.format("Accepted from [%s:%d]", remoteAddress.getAddress(), remoteAddress.getPort()));
+            }
+            bytesReceived = buf.position() - 1;
+        } else {
+            bytesReceived = channel.read(buf);
+        }
+        if (bytesReceived > 0) {
+            output.write(buf.array(), 0, buf.position());
+        }
+        return bytesReceived;
+    }
+}
--- src/netcat/StreamTransferer.java
+++ src/netcat/StreamTransferer.java
@@ -7,29 +7,26 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
-public class StreamTransferer implements Runnable
-{
+public class StreamTransferer implements Runnable {
     private InputStream input;
     private OutputStream output;
 
-    public StreamTransferer( InputStream input, OutputStream output )
-    {
+    public StreamTransferer(InputStream input, OutputStream output) {
         this.input = input;
         this.output = output;
     }
 
     @Override
-    public void run()
-    {
+    public void run() {
         try {
-            PrintWriter writer = new PrintWriter( output );
-            BufferedReader reader = new BufferedReader( new InputStreamReader( input ) );
+            PrintWriter writer = new PrintWriter(output);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
             String line;
-            while( ( line = reader.readLine() ) != null ) {
-                writer.println( line );
+            while ((line = reader.readLine()) != null) {
+                writer.println(line);
                 writer.flush();
             }
-        } catch( IOException e ) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
